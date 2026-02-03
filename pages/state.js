739079b3b38ef4441c457029063aa90871122d63
Claudio Costa <costa.pederneschi@gmail.com>
2026-02-03 12:45:34 -0300

    correção socket.on

--- pages/state.js
+++ pages/state.js
@@ -4,7 +4,7 @@ const SOCKET_URL = 'https://binancesocket.onrender.com';
 
 export default function paginastatus() {
     const socket = io(SOCKET_URL, { transports: ['websocket'] });
-    socket.onAny('state', (data) => {
+    socket.on('state', (data) => {
         console.log(data);
     })
 
